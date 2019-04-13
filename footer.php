87f4897b7b8ebe51874d22d572e1792ef8a4e6f3
TANISHA KUMBHAKAR <46387444+tanishakumbhakar@users.noreply.github.com>
2019-04-13 19:38:01 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,21 @@
+<footer class="footer-sky">
+        <div class="footer-mid">
+            <div class="container">
+                <div class="row padding-footer-mid">
+                    <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+                        <div class="footer-logo text-center list-content">
+                            <a href="home.php#" title="Skyline"><img src="./home/sky-logo-footer.png" alt="Image"></a>
+                        </div>
+                    </div>
+                    <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 col-lg-offset-1 col-md-offset-1  ">
+                        <div class="list-content">
+                            <ul>
+                                <li><a href="home.php#" title="">&copy; Django Nahi Aata,MNNIT Allahabad</a></li>
+                               
+                            </ul>
+                        </div>
+                    </div>
+					  </div>
+            </div>
+        </div>
+    </footer>
