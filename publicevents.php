62c5bb490245d2f8cc1158bf88c4b0ed056e695a
Siddhi-Mahawar <34399383+Siddhi-Mahawar@users.noreply.github.com>
2019-04-14 09:09:48 +0530

    Update publicevents.php

--- publicevents.php
+++ publicevents.php
@@ -120,12 +120,10 @@ used to vertically center elements, may need modification if you're not using de
     include 'header.php';
     include 'nav.php' ?>
 	
-        
-                    
-                 
+ <div id="space">
 <div class="container"  >
 
-   <div class="row" id="space">
+   <div class="row">
    <div class="col-md-10 col-md-offset-1">
 
             <div class="panel panel-default panel-table">
@@ -210,5 +208,5 @@ used to vertically center elements, may need modification if you're not using de
 						</tbody>
                         </table></div></div></div></div>
    </div>
-</div>
+	 </div></div>
 <?php include('footer.php');?>
